0ca83ef77892af2bb02ba3c28d5e862a92b62156
claushorn <28705786+claushorn@users.noreply.github.com>
2025-07-28 19:27:03 +0200

    env vars need VITE prefix

--- src/config/emailjs.ts
+++ src/config/emailjs.ts
@@ -2,5 +2,5 @@
 export const emailjsConfig = {
   serviceId: 'service_ngu6b1x',
   templateId: 'template_1ryfm2q',
-  publicKey: import.meta.env.EMAILJS_PUBLIC_KEY || '',
+  publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY || '',
 }; 
